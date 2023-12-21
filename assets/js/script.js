c9c3b415f2ea496aff66b76079111e1868725f98
sydbk7 <sydbk7@gmail.com>
2023-12-21 21:03:51 +0500

    Fixed toggle button in navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,15 @@
 
 const $navbar = document.querySelector('[data-navbar]')
 const $navToggler = document.querySelector('[data-nav-toggle]')
+const $openSpan = document.querySelector('.nav-toggle-btn .open')
+const $closeSpan = document.querySelector('.nav-toggle-btn .close')
 
-$navToggler.addEventListener('click', () => $navbar.classList.toggle('active'))
+$navToggler.addEventListener('click', () => {
+    $navbar.classList.toggle('active');
+    const isActive = $navbar.classList.contains('active');
+    $openSpan.style.display = isActive ? 'none' : 'block';
+    $closeSpan.style.display = isActive ? 'block' : 'none';
+});
 
 const $header = document.querySelector('[data-header]')
 
